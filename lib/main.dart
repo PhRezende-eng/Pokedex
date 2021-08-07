ac3d0516bbd993b20560c90cb22c803b6e7ecca5
PhRezende-eng <ph.hr.001@gmail.com>
2021-08-06 23:19:45 -0300

    add new photo on app pokedex

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pokedex',
       // theme: darkMode(context) ? ThemeData.dark() : ThemeData.light(),
       // theme: ThemeData.dark(),
       // theme: ThemeData.light(),
